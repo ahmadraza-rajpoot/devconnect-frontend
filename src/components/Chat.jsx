15d456bf09b0f8d3b3bd6caeba0d47ed88a2dddf
Ahmad Raza <razarajpoot99598@gmail.com>
2026-06-15 00:49:43 +0500

    implemented real time chat feature on DevConnect

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { createSocketConnection } from './utils/socket'
 
 const Chat = () => {
     const {targetUserId} = useParams()
-  return (
-    <div className='w-200 mx-auto'>
-        <h1 className='border-b p-3'>Chat - Have Fun</h1>
-        <div className='flex flex-col border'>
 
-            <div className=' h-100 border overflow-y-scroll p-4 '>
-                <div>
-                    <div className="chat-header">
-                        Obi-Wan Kenobi
-                        <time className="text-xs opacity-50">2 hours ago</time>
-                    </div>
-                    <div className="chat-bubble">You were the Chosen One!</div>
-                    <div className="chat-footer opacity-50">Seen</div>
-                </div>
+    const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState("")
+    const user = useSelector(store => store.user)
+    const userId = user?._id
+ 
+    useEffect(()=>{
+
+        if(!userId) return;
+
+        const socket = createSocketConnection();
+        
+        socket.emit("joinChat", {userName: user.firstName, userId, targetUserId})
+
+
+        socket.on("messageReceived", ({firstName, message})=>{
+
+            console.log(firstName + " " + message)
+            setMessages(msg => [...msg, {firstName, message}])
+        })
+
+        return () =>{
+            socket.disconnect()
+        }
+    },[userId, targetUserId])
+
+   
+
+    const sendMessage =()=>{
+        const socket = createSocketConnection()
+
+        socket.emit("sendMessage",{firstName:user.firstName, userId, targetUserId, message})
+        setMessage("")
+    }
+
+
+  return (
+    <div className='w-200 mx-auto mb-5'>
+        <h1 className='border-b border-gray-700 p-3'>Chat - Have Fun</h1>
+        <div className='flex flex-col border border-gray-700'>
+
+            <div className=' h-100 border border-gray-700 overflow-y-scroll p-4 '>
+                {messages.map((msg, idx)=>(<div>
+                        <div className="chat-header">
+                            {msg.firstName}
+                            <time className="text-xs opacity-50">2 hours ago</time>
+                        </div>
+                        <div className="chat-bubble">{msg.message}</div>
+                        <div className="chat-footer opacity-50">Seen</div>
+                    </div>))}
 
                 
                 
@@ -26,8 +64,8 @@ const Chat = () => {
             
 
             <div className='h-10 flex justify-between '>
-                <input className='border px-2 outline-none w-full ' placeholder='type your message here...' type="text"  />
-                <button className='btn btn-primary rounded-none'>Send</button>
+                <input value={message} onChange={(e)=>setMessage(e.target.value)} className='border border-gray-700 px-2 outline-none w-full ' placeholder='type your message here...' type="text"  />
+                <button onClick={sendMessage} className='btn btn-primary rounded-none'>Send</button>
             </div>
         </div>
     </div>
